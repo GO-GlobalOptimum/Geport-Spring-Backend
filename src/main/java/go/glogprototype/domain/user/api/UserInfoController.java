13f9c234505f53c47326a239b495ecca5eebbfb8
chungSungMin <taky0315@naver.com>
2024-05-28 21:22:45 +0900

    [ FEAT ] UserInfo router 수정

--- src/main/java/go/glogprototype/domain/user/api/UserInfoController.java
+++ src/main/java/go/glogprototype/domain/user/api/UserInfoController.java
@@ -6,7 +6,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/api/member")
+@RequestMapping("/spring/userInfo")
 @RequiredArgsConstructor
 public class UserInfoController {
 
