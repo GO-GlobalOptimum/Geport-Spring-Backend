bd4f6f13614821d32e279aa2a95d308f2c6665d7
chungSungMin <105360496+chungSungMin@users.noreply.github.com>
2024-05-29 19:08:51 +0900

    Update SecurityConfig.java

--- src/main/java/go/glogprototype/global/config/SecurityConfig.java
+++ src/main/java/go/glogprototype/global/config/SecurityConfig.java
@@ -1,8 +1,6 @@
 package go.glogprototype.global.config;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-
-
 import go.glogprototype.domain.user.dao.UserRepository;
 import go.glogprototype.global.jwt.filter.CustomJsonUsernamePasswordAuthenticationFilter;
 import go.glogprototype.global.jwt.filter.JwtAuthenticationProcessingFilter;
@@ -24,11 +22,6 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.logout.LogoutFilter;
 
-
-/**
- * 인증은 CustomJsonUsernamePasswordAuthenticationFilter에서 authenticate()로 인증된 사용자로 처리
- * JwtAuthenticationProcessingFilter는 AccessToken, RefreshToken 재발급
- */
 @Configuration
 @EnableWebSecurity
 @RequiredArgsConstructor
@@ -44,69 +37,32 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
-                .formLogin(formlogin->formlogin.disable()) // disable FormLogin
-                .httpBasic(httpBasic ->
-                        httpBasic.disable()
-                ) // disable httpBasic
-                .csrf(csrf->csrf.disable()) // disable csrf security
-                .headers((headersConfig) ->
-                        headersConfig.frameOptions(frameOptionsConfig ->
-                                frameOptionsConfig.disable()
-                        )
-                )
+                .formLogin(formLogin -> formLogin.disable()) // disable FormLogin
+                .httpBasic(httpBasic -> httpBasic.disable()) // disable httpBasic
+                .csrf(csrf -> csrf.disable()) // disable csrf security
+                .headers(headersConfig -> headersConfig.frameOptions(frameOptionsConfig -> frameOptionsConfig.disable()))
 
-                // Since session is not used, set to STATELESS
-                .sessionManagement(sessionManagement ->
-                        sessionManagement.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-                )
+                // Set session management to stateless
+                .sessionManagement(sessionManagement -> sessionManagement.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
 
-                //== Permission management options per URL ==//
-                .authorizeHttpRequests(authorize ->
-                        authorize
-//                                .requestMatchers("/", "/css/**", "/images/**", "/js/**", "/favicon.ico", "/h2-console/**").permitAll()
-                                .requestMatchers("/sign-up","/login","/swagger-ui/index.html/**",   "/api-docs",
-                                        "/swagger-ui-custom.html",
-                                        "/v3/api-docs/**",
-                                        "/swagger-ui/**",
-                                        "/api-docs/**",
-                                        "/swagger-ui.html",
-                                        "/swagger-custom-ui.html").permitAll() // Access to membership registration
-                                .anyRequest().authenticated() // Only authenticated users can access all paths other than the above
+                // Permission management per URL
+                .authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers("/sign-up", "/login", "/swagger-ui/index.html/**", "/api-docs", "/swagger-ui-custom.html",
+                                "/v3/api-docs/**", "/swagger-ui/**", "/api-docs/**", "/swagger-ui.html", "/swagger-custom-ui.html").permitAll()
+                        .anyRequest().authenticated())
 
-
-                )
-
-//                .authorizeRequests(authorize ->
-//                        authorize
-//                                .requestMatchers("/", "/css/**", "/images/**", "/js/**", "/favicon.ico", "/h2-console/**").permitAll()
-//                                .requestMatchers(new AntPathRequestMatcher("/oauth2/authorization/google")).permitAll()
-//                                .requestMatchers(new AntPathRequestMatcher("/sign-up")).permitAll()// Access to membership registration
-//                                .anyRequest().authenticated() // Only authenticated users can access all paths other than the above
-//
-//
-//                )
-
-                .oauth2Login((oauth2)->oauth2
+                // OAuth2 Login Configuration
+                .oauth2Login(oauth2 -> oauth2
                         .authorizationEndpoint(authorizationEndpoint -> authorizationEndpoint
-                                        .baseUri("/oauth2/authorization/google") //여기로 인증 요청이 들어옴. OAuth2 인증 요청이 시작되는 엔드포인트, Google 로그인 버튼을 눌렀을 때 이 엔드포인트로 이동
-                                //.authorizationRequestRepository(cookieAuthorizationRequestRepository)
-                        )
+                                .baseUri("/spring/oauth2/authorization/google"))
                         .redirectionEndpoint(redirectionEndpoint -> redirectionEndpoint
-                                .baseUri("/oauth2/code/google") //authorization url을 통한 인증 결과에 따라 redirect 되는 url. 인증이 정상적으로 진행되는 경우에는 callback url으로 사용자 인증 코드(authorization code)를 함께 반환됨.
-                        )
+                                .baseUri("/spring/oauth2/code/google"))
                         .successHandler(oAuth2LoginSuccessHandler)
                         .failureHandler(oAuth2LoginFailureHandler)
                         .userInfoEndpoint(userInfoEndpoint -> userInfoEndpoint.userService(customOAuth2UserService))
                 );
 
-        // icon, css, js related
-        // All materials in the basic page, css, image, and js subfolders are accessible, and can be accessed in h2-console.
-
-
-
-        // 원래 스프링 시큐리티 필터 순서가 LogoutFilter 이후에 로그인 필터 동작
-        // 따라서, LogoutFilter 이후에 우리가 만든 필터 동작하도록 설정
-        // 순서 : LogoutFilter -> JwtAuthenticationProcessingFilter -> CustomJsonUsernamePasswordAuthenticationFilter
+        // Add custom filters
         http.addFilterAfter(customJsonUsernamePasswordAuthenticationFilter(), LogoutFilter.class);
         http.addFilterBefore(jwtAuthenticationProcessingFilter(), CustomJsonUsernamePasswordAuthenticationFilter.class);
 
@@ -118,86 +74,22 @@ public class SecurityConfig {
         return PasswordEncoderFactories.createDelegatingPasswordEncoder();
     }
 
-    /**
-     * AuthenticationManager 설정 후 등록
-     * PasswordEncoder를 사용하는 AuthenticationProvider 지정 (PasswordEncoder는 위에서 등록한 PasswordEncoder 사용)
-     * FormLogin(기존 스프링 시큐리티 로그인)과 동일하게 DaoAuthenticationProvider 사용
-     * UserDetailsService는 커스텀 LoginService로 등록
-     * 또한, FormLogin과 동일하게 AuthenticationManager로는 구현체인 ProviderManager 사용(return ProviderManager)
-     *
-     */
     @Bean
     public AuthenticationManager authenticationManager() {
         DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
         provider.setPasswordEncoder(passwordEncoder());
-//        provider.setUserDetailsService(loginService);
         return new ProviderManager(provider);
     }
 
-    /**
-     * 로그인 성공 시 호출되는 LoginSuccessJWTProviderHandler 빈 등록
-     */
-//    @Bean
-//    public LoginSuccessHandler loginSuccessHandler() {
-//        return new LoginSuccessHandler(jwtService, userRepository);
-//    }
-//
-//    /**
-//     * 로그인 실패 시 호출되는 LoginFailureHandler 빈 등록
-//     */
-//    @Bean
-//    public LoginFailureHandler loginFailureHandler() {
-//        return new LoginFailureHandler();
-//    }
-
-    /**
-     * CustomJsonUsernamePasswordAuthenticationFilter 빈 등록
-     * 커스텀 필터를 사용하기 위해 만든 커스텀 필터를 Bean으로 등록
-     * setAuthenticationManager(authenticationManager())로 위에서 등록한 AuthenticationManager(ProviderManager) 설정
-     * 로그인 성공 시 호출할 handler, 실패 시 호출할 handler로 위에서 등록한 handler 설정
-     */
     @Bean
     public CustomJsonUsernamePasswordAuthenticationFilter customJsonUsernamePasswordAuthenticationFilter() {
-        CustomJsonUsernamePasswordAuthenticationFilter customJsonUsernamePasswordLoginFilter
-                = new CustomJsonUsernamePasswordAuthenticationFilter(objectMapper);
-        customJsonUsernamePasswordLoginFilter.setAuthenticationManager(authenticationManager());
-//        customJsonUsernamePasswordLoginFilter.setAuthenticationSuccessHandler(loginSuccessHandler());
-//        customJsonUsernamePasswordLoginFilter.setAuthenticationFailureHandler(loginFailureHandler());
-        return customJsonUsernamePasswordLoginFilter;
+        CustomJsonUsernamePasswordAuthenticationFilter filter = new CustomJsonUsernamePasswordAuthenticationFilter(objectMapper);
+        filter.setAuthenticationManager(authenticationManager());
+        return filter;
     }
-
 
     @Bean
     public JwtAuthenticationProcessingFilter jwtAuthenticationProcessingFilter() {
-        JwtAuthenticationProcessingFilter jwtAuthenticationFilter = new JwtAuthenticationProcessingFilter(jwtService, userRepository);
-        return jwtAuthenticationFilter;
+        return new JwtAuthenticationProcessingFilter(jwtService, userRepository);
     }
-
-
-    //Swagger 설정
-
-//    @Bean
-//    public OpenAPI openAPI() {
-//        String jwt = "JWT";
-//        SecurityRequirement securityRequirement = new SecurityRequirement().addList(jwt);
-//        Components components = new Components().addSecuritySchemes(jwt, new SecurityScheme()
-//                .name(jwt)
-//                .type(SecurityScheme.Type.HTTP)
-//                .scheme("bearer")
-//                .bearerFormat("JWT")
-//        );
-//        return new OpenAPI()
-//                .components(new Components())
-//                .info(apiInfo())
-//                .addSecurityItem(securityRequirement)
-//                .components(components);
-//    }
-//    private Info apiInfo() {
-//        return new Info()
-//                .title("API Test") // API의 제목
-//                .description("Let's practice Swagger UI") // API에 대한 설명
-//                .version("1.0.0"); // API의 버전
-//    }
-
-
 }
