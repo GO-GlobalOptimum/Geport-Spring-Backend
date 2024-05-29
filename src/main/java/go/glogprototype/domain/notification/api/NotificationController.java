4a62cc8e6c727f98610a948feb32058e8c9e23b8
jione-park <pich7755@naver.com>
2024-05-29 17:42:07 +0900

    [FIX] member -> user refactoring

--- src/main/java/go/glogprototype/domain/notification/api/NotificationController.java
+++ src/main/java/go/glogprototype/domain/notification/api/NotificationController.java
@@ -1,26 +1,14 @@
 package go.glogprototype.domain.notification.api;
 
 import go.glogprototype.domain.notification.application.NotificationService;
-import go.glogprototype.domain.post.application.PostService;
-import go.glogprototype.domain.post.dto.CreatePostRequestDto;
-import go.glogprototype.domain.post.dto.CreatePostResponseDto;
-import go.glogprototype.domain.post.dto.PostDto.*;
-import go.glogprototype.domain.user.dao.MemberRepository;
-import go.glogprototype.domain.user.domain.Member;
-import jakarta.servlet.http.HttpServletRequest;
+import go.glogprototype.domain.user.dao.UserRepository;
 import lombok.RequiredArgsConstructor;
 
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
-
-import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -29,12 +17,12 @@ import java.util.List;
 public class NotificationController {
 
     private final NotificationService notificationService;
-    private final MemberRepository memberRepository;
+    private final UserRepository userRepository;
 
     @GetMapping("/subscribe")
     public SseEmitter subscribe(@AuthenticationPrincipal UserDetails userDetails) {
         String userEmail = userDetails.getUsername(); // UserDetails에서 userId 추출하는 메소드
-        Long userId = memberRepository.findByEmail(userEmail)
+        Long userId = userRepository.findByEmail(userEmail)
                 .orElseThrow(() -> new IllegalArgumentException("No member found with email: " + userEmail))
                 .getId(); // 이메일에 해당하는 멤버의 ID 추출
 
--- src/main/java/go/glogprototype/domain/user/application/FollowService.java
+++ src/main/java/go/glogprototype/domain/user/application/FollowService.java
@@ -13,13 +13,13 @@ import org.springframework.transaction.annotation.Transactional;
 @RequiredArgsConstructor
 public class FollowService {
 
-    private final UserRepository memberRepository;
+    private final UserRepository userRepository;
     private final FollowRepository followRepository;
 
     public void followUser(String followerEmail, String followingEmail) {
-        Member follower = memberRepository.findByEmail(followerEmail)
+        Member follower = userRepository.findByEmail(followerEmail)
                 .orElseThrow(() -> new IllegalArgumentException("No member found with email: " + followerEmail));
-        Member following = memberRepository.findByEmail(followingEmail)
+        Member following = userRepository.findByEmail(followingEmail)
                 .orElseThrow(() -> new IllegalArgumentException("No member found with email: " + followingEmail));
 
         if (!followRepository.existsByFollowerAndFollowing(follower, following)) {
@@ -31,15 +31,15 @@ public class FollowService {
 
             follower.setFollowingCount(follower.getFollowingCount() + 1);
             following.setFollowerCount(following.getFollowerCount() + 1);
-            memberRepository.save(follower);
-            memberRepository.save(following);
+            userRepository.save(follower);
+            userRepository.save(following);
         }
     }
 
     public void unfollowUser(String followerEmail, String followingEmail) {
-        Member follower = memberRepository.findByEmail(followerEmail)
+        Member follower = userRepository.findByEmail(followerEmail)
                 .orElseThrow(() -> new IllegalArgumentException("No member found with email: " + followerEmail));
-        Member following = memberRepository.findByEmail(followingEmail)
+        Member following = userRepository.findByEmail(followingEmail)
                 .orElseThrow(() -> new IllegalArgumentException("No member found with email: " + followingEmail));
 
         if (followRepository.existsByFollowerAndFollowing(follower, following)) {
@@ -47,8 +47,8 @@ public class FollowService {
 
             follower.setFollowingCount(follower.getFollowingCount() - 1);
             following.setFollowerCount(following.getFollowerCount() - 1);
-            memberRepository.save(follower);
-            memberRepository.save(following);
+            userRepository.save(follower);
+            userRepository.save(following);
         }
     }
 }
