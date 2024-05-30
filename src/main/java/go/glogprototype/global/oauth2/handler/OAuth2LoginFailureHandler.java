1fcb0d0f1f6ec5a22d607a2ad1dc8618a7fc9a9f
chungSungMin <taky0315@naver.com>
2024-05-30 21:05:14 +0900

    [ FIX ] 로그인 실패 로그 찍기 추가

--- src/main/java/go/glogprototype/global/oauth2/handler/OAuth2LoginFailureHandler.java
+++ src/main/java/go/glogprototype/global/oauth2/handler/OAuth2LoginFailureHandler.java
@@ -17,6 +17,11 @@ public class OAuth2LoginFailureHandler implements AuthenticationFailureHandler {
     public void onAuthenticationFailure(HttpServletRequest request, HttpServletResponse response, AuthenticationException exception) throws IOException, ServletException {
         response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
         response.getWriter().write("소셜 로그인 실패! 서버 로그를 확인해주세요.");
-        log.info("소셜 로그인에 실패했습니다. 에러 메시지 : {}", exception.getMessage());
+
+        // 로그 출력
+        log.error("소셜 로그인에 실패했습니다. 에러 메시지 : {}", exception.getMessage());
+        log.debug("Request URI: {}", request.getRequestURI());
+        log.debug("Query String: {}", request.getQueryString());
+        log.debug("Exception: ", exception);
     }
 }
