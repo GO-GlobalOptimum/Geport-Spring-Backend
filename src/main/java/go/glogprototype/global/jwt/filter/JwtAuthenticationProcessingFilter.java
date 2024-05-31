1b5df108a57c625b9c7729d0ab623d79c8ad7d88
jiwon_park <105265104+jione-park@users.noreply.github.com>
2024-05-31 10:55:08 +0900

    Update JwtAuthenticationProcessingFilter.java

--- src/main/java/go/glogprototype/global/jwt/filter/JwtAuthenticationProcessingFilter.java
+++ src/main/java/go/glogprototype/global/jwt/filter/JwtAuthenticationProcessingFilter.java
@@ -39,7 +39,7 @@ import java.util.Optional;
 @Slf4j
 public class JwtAuthenticationProcessingFilter extends OncePerRequestFilter {
 
-    private static final String NO_CHECK_URL = "/login"; // "/login"으로 들어오는 요청은 Filter 작동 X
+    private static final String NO_CHECK_URL = "/spring/login"; // "/login"으로 들어오는 요청은 Filter 작동 X
 
     private final JwtService jwtService;
     private final UserRepository userRepository;
