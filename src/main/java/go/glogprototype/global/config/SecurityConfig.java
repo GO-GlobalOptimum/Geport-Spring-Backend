e506279bb7a2d2a6a11f03a81f3283970d55227c
Junbum Choi <117131532+Junbum-hub@users.noreply.github.com>
2024-05-31 15:40:51 +0900

    Update SecurityConfig.java

--- src/main/java/go/glogprototype/global/config/SecurityConfig.java
+++ src/main/java/go/glogprototype/global/config/SecurityConfig.java
@@ -45,8 +45,8 @@ public class SecurityConfig {
                 .headers(headers -> headers.frameOptions(frameOptions -> frameOptions.disable()))
                 .sessionManagement(sessionManagement -> sessionManagement.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(authorize -> authorize
-                        .requestMatchers("/sign-up", "/spring/login", "/swagger-ui/index.html/**", "/api-docs", "/swagger-ui-custom.html",
-                                "/v3/api-docs/**", "/swagger-ui/**", "/api-docs/**", "/swagger-ui.html", "/swagger-custom-ui.html", "/spring/posts/test", "/spring/login/oauth2/code/google").permitAll()
+                        .requestMatchers("/sign-up", "/login", "/swagger-ui/index.html/**", "/api-docs", "/swagger-ui-custom.html",
+                                "/v3/api-docs/**", "/swagger-ui/**", "/api-docs/**", "/swagger-ui.html", "/swagger-custom-ui.html", "/spring/posts/test", "/login/oauth2/code/google").permitAll()
                         .anyRequest().authenticated()
                 )
                 .oauth2Login(oauth2 -> oauth2
