c653685c158ec91231d5b973384777bb329172a4
jiwon_park <105265104+jione-park@users.noreply.github.com>
2024-05-31 11:49:52 +0900

    Update OAuth2LoginSuccessHandler.java

--- src/main/java/go/glogprototype/global/oauth2/handler/OAuth2LoginSuccessHandler.java
+++ src/main/java/go/glogprototype/global/oauth2/handler/OAuth2LoginSuccessHandler.java
@@ -30,6 +30,7 @@ public class OAuth2LoginSuccessHandler implements AuthenticationSuccessHandler {
     @Override
     public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
         log.info("OAuth2 Login 성공!");
+        //태스트
         try {
             CustomOAuth2User oAuth2User = (CustomOAuth2User) authentication.getPrincipal();
 
