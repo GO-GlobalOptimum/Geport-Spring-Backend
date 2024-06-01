872df4d2affa63407f48b2ba6dabe4983b08ff0c
chungSungMin <105360496+chungSungMin@users.noreply.github.com>
2024-06-02 00:38:00 +0900

    Create ViewRepository.java

---
+++ src/main/java/go/glogprototype/domain/user/dao/ViewRepository.java
@@ -0,0 +1,11 @@
+package go.glogprototype.domain.user.dao;
+
+import go.glogprototype.domain.user.domain.View;
+import go.glogprototype.domain.post.domain.Post;
+import go.glogprototype.domain.user.domain.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.Optional;
+
+public interface ViewRepository extends JpaRepository<View, Long> {
+    Optional<View> findByPostAndMember(Post post, Member member);
+}
