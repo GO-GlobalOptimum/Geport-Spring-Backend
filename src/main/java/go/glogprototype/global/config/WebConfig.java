1e903fde59593371b4382efa37a7c9ea6efd25e9
chungSungMin <105360496+chungSungMin@users.noreply.github.com>
2024-06-03 18:28:13 +0900

    Update WebConfig.java

--- src/main/java/go/glogprototype/global/config/WebConfig.java
+++ src/main/java/go/glogprototype/global/config/WebConfig.java
@@ -13,7 +13,7 @@ public class WebConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         // 모든 경로에 대해 모든 HTTP 메서드에 대한 CORS를 허용
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:8080", "http://localhost:4000", "http://geport.blog", "https://geport.blog")
+                .allowedOrigins("http://localhost:8080", "http://localhost:3000", "http://geport.blog", "https://geport.blog") // 여기서 특정 출처를 명시합니다.
                 .allowedMethods("GET", "POST", "PUT", "DELETE")
                 .allowCredentials(true)
                 .allowedHeaders("*");
