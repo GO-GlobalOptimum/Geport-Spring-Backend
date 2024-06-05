41e07b898f94e27dc47fe4711c49831b6a0438ce
jione-park <pich7755@naver.com>
2024-06-05 20:38:16 +0900

    GlogPrototypeApplication 수정

--- src/main/java/go/glogprototype/GlogPrototypeApplication.java
+++ src/main/java/go/glogprototype/GlogPrototypeApplication.java
@@ -3,9 +3,10 @@ package go.glogprototype;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
-@EnableJpaAuditing
+@EnableJpaRepositories(basePackages = "go.glogprototype.domain")
 public class GlogPrototypeApplication {
 
     public static void main(String[] args) {
