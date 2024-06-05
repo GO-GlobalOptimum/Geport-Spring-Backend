1f2e730f1386b3d0ea2e0ef2b832f7a2a0bc3d76
jione-park <pich7755@naver.com>
2024-06-06 01:39:35 +0900

    [update] cors config 수정

--- src/main/java/go/glogprototype/global/config/CorsConfig.java
+++ src/main/java/go/glogprototype/global/config/CorsConfig.java
@@ -16,7 +16,7 @@ public class CorsConfig {
         CorsConfiguration config = new CorsConfiguration();
 
         config.setAllowCredentials(true);
-        config.setAllowedOrigins(List.of("*"));
+        config.setAllowedOrigins(List.of("https://geport.blog"));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
         config.setExposedHeaders(List.of("*"));
