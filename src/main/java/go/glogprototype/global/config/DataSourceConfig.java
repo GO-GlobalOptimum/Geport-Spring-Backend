34aaafd3b5206433fbcf97b2adb8f282656a0c4f
jione-park <pich7755@naver.com>
2024-06-05 17:12:18 +0900

    [update] add dataconfig files

---
+++ src/main/java/go/glogprototype/global/config/DataSourceConfig.java
@@ -0,0 +1,73 @@
+package go.glogprototype.global.config;
+
+import com.zaxxer.hikari.HikariDataSource;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.lookup.AbstractRoutingDataSource;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+
+import jakarta.persistence.EntityManagerFactory;
+import javax.sql.DataSource;
+import java.util.HashMap;
+import java.util.Map;
+
+@Configuration
+public class DataSourceConfig {
+
+    @Bean(name = "writeDataSourceProperties")
+    public DataSourceProperties writeDataSourceProperties() {
+        return new DataSourceProperties();
+    }
+
+    @Bean(name = "readDataSourceProperties")
+    public DataSourceProperties readDataSourceProperties() {
+        return new DataSourceProperties();
+    }
+
+    @Bean(name = "writeDataSource")
+    public DataSource writeDataSource(@Qualifier("writeDataSourceProperties") DataSourceProperties writeDataSourceProperties) {
+        return writeDataSourceProperties.initializeDataSourceBuilder().type(HikariDataSource.class).build();
+    }
+
+    @Bean(name = "readDataSource")
+    public DataSource readDataSource(@Qualifier("readDataSourceProperties") DataSourceProperties readDataSourceProperties) {
+        return readDataSourceProperties.initializeDataSourceBuilder().type(HikariDataSource.class).build();
+    }
+
+    @Bean
+    public AbstractRoutingDataSource routingDataSource(@Qualifier("writeDataSource") DataSource writeDataSource,
+                                                       @Qualifier("readDataSource") DataSource readDataSource) {
+        AbstractRoutingDataSource routingDataSource = new AbstractRoutingDataSource() {
+            @Override
+            protected Object determineCurrentLookupKey() {
+                return DataSourceContextHolder.getDataSourceType();
+            }
+        };
+
+        Map<Object, Object> targetDataSources = new HashMap<>();
+        targetDataSources.put(DataSourceType.WRITE, writeDataSource);
+        targetDataSources.put(DataSourceType.READ, readDataSource);
+        routingDataSource.setTargetDataSources(targetDataSources);
+        routingDataSource.setDefaultTargetDataSource(writeDataSource);
+
+        return routingDataSource;
+    }
+
+    @Bean
+    public LocalContainerEntityManagerFactoryBean entityManagerFactory(AbstractRoutingDataSource routingDataSource) {
+        LocalContainerEntityManagerFactoryBean entityManagerFactory = new LocalContainerEntityManagerFactoryBean();
+        entityManagerFactory.setDataSource(routingDataSource);
+        entityManagerFactory.setPackagesToScan("go.glogprototype.domain.post.domain", "go.glogprototype.domain.user.domain");
+        entityManagerFactory.setJpaVendorAdapter(new HibernateJpaVendorAdapter());
+        return entityManagerFactory;
+    }
+
+    @Bean
+    public JpaTransactionManager transactionManager(EntityManagerFactory entityManagerFactory) {
+        return new JpaTransactionManager(entityManagerFactory);
+    }
+}
---
+++ src/main/java/go/glogprototype/global/config/DataSourceType.java
@@ -0,0 +1,5 @@
+package go.glogprototype.global.config;
+
+public enum DataSourceType {
+    READ, WRITE
+}
